57d6bace9144faec169e1e18cb76060d73d40182
ayoub oumha <ayyouboumha@gmail.com>
2024-11-14 09:00:17 +0100

    commit

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,6 +1,9 @@
-async function init() {
-    await loadQuestions() ;
-    console.log(questionsArray)
-    
-}
-init()
+ timer.textContent =12 ;
+    NumberOfQuestion.textContent = 100 ;
+   
+    divOfOptions.innerHTML = `<button   
+                class="answers w-full max-w-96 text-start mb-3 pl-5 h-9 rounded-full border-2 border-[#BFCFE7] hover:text-white hover:bg-[#525CEB] hover:border-[#525CEB]"
+                disabled=""
+              >
+                a- <span>Elle</span>
+              </button>`
