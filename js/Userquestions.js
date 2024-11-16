534407a2f39960fc2418dbec108f1d8f154426c7
ayoub oumha <ayyouboumha@gmail.com>
2024-11-16 19:10:34 +0100

    last commit

--- js/Userquestions.js
+++ js/Userquestions.js
@@ -16,8 +16,7 @@ function selectQuestions() {
   let BtnNevA1 = document.getElementById("testA1");
   let BtnNevA2 = document.getElementById("testA2");
   let BtnNevB1 = document.getElementById("testB1");
-  BtnNevA2.disabled = false;
-  BtnNevB1.disabled = false;
+
   // btn of all categores
   let Btncate = document.querySelectorAll(".categore");
   // div of all catefore
@@ -26,24 +25,22 @@ function selectQuestions() {
     nevue = 0;
     nevueName = "A1";
     divOFCate.classList.remove("hidden");
-    displayCategoryScores() 
-    
+    displayCategoryScores();
+    calculateAndSaveTotalScore();
   });
   BtnNevA2.addEventListener("click", () => {
     nevue = 1;
     nevueName = "A2";
     divOFCate.classList.remove("hidden");
-    displayCategoryScores() 
-
-    
+    displayCategoryScores();
+    calculateAndSaveTotalScore();
   });
   BtnNevB1.addEventListener("click", () => {
     nevue = 2;
     nevueName = "B1";
     divOFCate.classList.remove("hidden");
-    displayCategoryScores() 
-
-    
+    displayCategoryScores();
+    calculateAndSaveTotalScore();
   });
   Btncate[0].addEventListener("click", () => {
     categorie = "Grammaire";
@@ -118,8 +115,9 @@ function AfficheeQuestion() {
 
   let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
   //   console.log(questionsFromLocalsrotage[nevue].categories[categorie][indexOFQ]);
-  
-  const totalQuestions = questionsFromLocalsrotage[nevue].categories[categorie].length;
+
+  const totalQuestions =
+    questionsFromLocalsrotage[nevue].categories[categorie].length;
 
   NumberOfQuestion.textContent = numbreOfQuestion + "/" + totalQuestions;
   QuestionDiv.textContent =
@@ -150,31 +148,31 @@ function ChoosetheAnswer() {
       );
 
       const span = ele.querySelector("span");
-      if (numbreOfQuestion == 1){
-        noteOFuser = 0 ;
+      if (numbreOfQuestion == 1) {
+        noteOFuser = 0;
       }
-      //     
+      //
       if (
         span.textContent ==
-        questionsFromLocalStorage[nevue].categories[categorie][indexOFQ].answer 
+        questionsFromLocalStorage[nevue].categories[categorie][indexOFQ].answer
       ) {
         noteOFuser++;
       }
 
       //     LocalStorage
       saveanswerinLocalstorage(span.textContent);
-      const totalQuestions = questionsFromLocalStorage[nevue].categories[categorie].length;
-      //      
+      const totalQuestions =
+        questionsFromLocalStorage[nevue].categories[categorie].length;
+      //
       if (numbreOfQuestion === totalQuestions) {
         clearInterval(intervalId);
-        afficherResult(); //  
+        afficherResult(); //
       } else {
-        goTonextQuetion(); 
+        goTonextQuetion();
       }
     });
   });
 }
-
 
 function saveanswerinLocalstorage(chosenAnswer) {
   let userAccount = JSON.parse(localStorage.getItem("userAccount"));
@@ -182,43 +180,42 @@ function saveanswerinLocalstorage(chosenAnswer) {
   let QuestionDiv = document.querySelector(".QuestionDiv");
   let timer = document.getElementById("timer");
 
-  const index = userAccount.findIndex(
-    (user) => user.username === username
-  );
+  const index = userAccount.findIndex((user) => user.username === username);
 
   if (numbreOfQuestion == 1) {
-    if (
-      userAccount[index].levels[nevueName][categorie].responses.length > 0
-    ) {
+    if (userAccount[index].levels[nevueName][categorie].responses.length > 0) {
       tentative++;
       userAccount[index].levels[nevueName][categorie].tentativeCat++;
-      userAccount[index].levels[nevueName].date = new Date().toLocaleDateString();
+      userAccount[index].levels[nevueName].date =
+        new Date().toLocaleDateString();
       userAccount[index].levels[nevueName].tentative++;
     }
 
     userAccount[index].levels[nevueName][categorie].responses = [];
     userAccount[index].levels[nevueName].noteNiveau -=
       userAccount[index].levels[nevueName][categorie].noteCat;
-    
+  }
+  if (
+    userAccount[index].levels[nevueName][categorie].responses.length ===
+    userAccount[index].levels[nevueName][categorie].noteCat
+  ) {
+    userAccount[index].levels[nevueName][categorie].valide = true;
+  } else {
+    userAccount[index].levels[nevueName][categorie].valide = false;
   }
 
-  
-  
-
-  
   userAccount[index].levels[nevueName][categorie].noteCat = noteOFuser;
 
-  
   let infoOfQuestion = {
     questionId: parseInt(QuestionDiv.id),
     chosenAnswer: chosenAnswer,
     time: 7 - parseInt(timer.textContent),
   };
 
-  
-  userAccount[index].levels[nevueName][categorie].responses.push(infoOfQuestion);
+  userAccount[index].levels[nevueName][categorie].responses.push(
+    infoOfQuestion
+  );
 
-  
   localStorage.setItem("userAccount", JSON.stringify(userAccount));
 }
 
@@ -226,15 +223,18 @@ function Timer() {
   let timer = document.getElementById("timer");
   timer.textContent = Chrono;
   Chrono--;
-  if (Chrono < 0) { 
-    let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
-    const totalQuestions = questionsFromLocalsrotage[nevue].categories[categorie].length;
+  if (Chrono < 0) {
+    let questionsFromLocalsrotage = JSON.parse(
+      localStorage.getItem("questions")
+    );
+    const totalQuestions =
+      questionsFromLocalsrotage[nevue].categories[categorie].length;
 
     if (numbreOfQuestion === totalQuestions) {
       clearInterval(intervalId);
-      afficherResult()
+      afficherResult();
     } else {
-      saveTimeoutData() ;
+      saveTimeoutData();
       goTonextQuetion();
     }
   }
@@ -252,11 +252,10 @@ function goTonextQuetion() {
   AfficheeQuestion();
 }
 
-
 function afficherResult() {
   // Get the container that holds the quiz
   let divOfQuiz = document.getElementById("quiz_container");
-  
+
   // Assume data exists in LocalStorage
   let userAccount = JSON.parse(localStorage.getItem("userAccount"));
   let username = localStorage.getItem("nameOFuser");
@@ -264,9 +263,10 @@ function afficherResult() {
 
   // Retrieve the score and level for the current user
   let noteNiveau = userAccount[index].levels[nevueName][categorie].noteCat || 0;
-  let level = nevueName || "Unknown" ;
+  let level = nevueName || "Unknown";
   let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
-  const totalQuestions = questionsFromLocalsrotage[nevue].categories[categorie].length;
+  const totalQuestions =
+    questionsFromLocalsrotage[nevue].categories[categorie].length;
 
   // Replace the quiz content with the results display
   divOfQuiz.innerHTML = `
@@ -303,94 +303,175 @@ function afficherResult() {
   });
 }
 
-
-
-
 function saveTimeoutData() {
   let userAccount = JSON.parse(localStorage.getItem("userAccount"));
   let username = localStorage.getItem("nameOFuser");
   let QuestionDiv = document.querySelector(".QuestionDiv");
-  
-  const index = userAccount.findIndex(
-      (user) => user.username === username
-  );
+
+  const index = userAccount.findIndex((user) => user.username === username);
 
   // If it's first question, handle attempt counting
   if (numbreOfQuestion == 1) {
-      if (userAccount[index].levels[nevueName][categorie].responses.length > 0) {
-          
-          userAccount[index].levels[nevueName][categorie].tentativeCat++
-          userAccount[index].levels[nevueName].date = new Date().toLocaleDateString();
-          
-      }
-      userAccount[index].levels[nevueName][categorie].responses = [];
-      
-      noteOFuser = 0;
+    if (userAccount[index].levels[nevueName][categorie].responses.length > 0) {
+      userAccount[index].levels[nevueName][categorie].tentativeCat++;
+      userAccount[index].levels[nevueName].date =
+        new Date().toLocaleDateString();
+    }
+    userAccount[index].levels[nevueName][categorie].responses = [];
+
+    noteOFuser = 0;
+  }
+  if (
+    userAccount[index].levels[nevueName][categorie].responses.length ===
+    userAccount[index].levels[nevueName][categorie].noteCat
+  ) {
+    userAccount[index].levels[nevueName][categorie].valide = true;
+  } else {
+    userAccount[index].levels[nevueName][categorie].valide = false;
   }
 
-  
   userAccount[index].levels[nevueName][categorie].noteCat = noteOFuser;
 
   // Save timeout response
   let infoOfquestion = {
-      questionId: parseInt(QuestionDiv.id),
-      chosenAnswer: "",  // Indicating no answer was selected
-      time: 7  // Full time used
+    questionId: parseInt(QuestionDiv.id),
+    chosenAnswer: "", // Indicating no answer was selected
+    time: 7, // Full time used
   };
-  
-  userAccount[index].levels[nevueName][categorie].responses.push(infoOfquestion);
+
+  userAccount[index].levels[nevueName][categorie].responses.push(
+    infoOfquestion
+  );
   localStorage.setItem("userAccount", JSON.stringify(userAccount));
 }
 
 function displayCategoryScores() {
-    // Get user data from localStorage
-    const userAccount = JSON.parse(localStorage.getItem("userAccount"));
-    const username = localStorage.getItem("nameOFuser");
-    const userIndex = userAccount.findIndex(user => user.username === username);
-    
-    // Get buttons
-    const grammaireBtn = document.getElementById("grammaire");
-    const vocabulaireBtn = document.getElementById("vocabulaire");
-    const comprehensionBtn = document.getElementById("comprehension");
-    let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
-     
-    // Only proceed if we have a valid user and level selected
-    grammaireBtn.textContent = "A1- Grammaire" + " " + userAccount[userIndex].levels[nevueName].Grammaire.noteCat + "/" + questionsFromLocalsrotage[nevue].categories.Grammaire.length  ;
-    vocabulaireBtn.textContent = "A1- Vocabulaire" + " " + userAccount[userIndex].levels[nevueName].Vocabulaire.noteCat + "/" + questionsFromLocalsrotage[nevue].categories.Vocabulaire.length  ;
-    comprehensionBtn.textContent = "A1- Compréhension" + " " + userAccount[userIndex].levels[nevueName].Compréhension.noteCat  + "/" + questionsFromLocalsrotage[nevue].categories.Compréhension.length  ;
+  // Get user data from localStorage
+  const userAccount = JSON.parse(localStorage.getItem("userAccount"));
+  const username = localStorage.getItem("nameOFuser");
+  const userIndex = userAccount.findIndex((user) => user.username === username);
 
-   
-    console.log(userAccount[userIndex].levels[nevueName] ) ; 
+  // Get buttons
+  const grammaireBtn = document.getElementById("grammaire");
+  const vocabulaireBtn = document.getElementById("vocabulaire");
+  const comprehensionBtn = document.getElementById("comprehension");
+  let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
+
+  // Only proceed if we have a valid user and level selected
+  grammaireBtn.textContent =
+    Object.keys(userAccount[userIndex].levels)[nevue] +
+    "- Grammaire" +
+    " " +
+    userAccount[userIndex].levels[nevueName].Grammaire.noteCat +
+    "/" +
+    questionsFromLocalsrotage[nevue].categories.Grammaire.length;
+  vocabulaireBtn.textContent =
+    Object.keys(userAccount[userIndex].levels)[nevue] +
+    "- Vocabulaire" +
+    " " +
+    userAccount[userIndex].levels[nevueName].Vocabulaire.noteCat +
+    "/" +
+    questionsFromLocalsrotage[nevue].categories.Vocabulaire.length;
+  comprehensionBtn.textContent =
+    Object.keys(userAccount[userIndex].levels)[nevue] +
+    "- Compréhension" +
+    " " +
+    userAccount[userIndex].levels[nevueName].Compréhension.noteCat +
+    "/" +
+    questionsFromLocalsrotage[nevue].categories.Compréhension.length;
 }
-
-
-
 
 function calculateAndSaveTotalScore() {
   // Get data from localStorage
   let userAccount = JSON.parse(localStorage.getItem("userAccount"));
   let username = localStorage.getItem("nameOFuser");
-  
-  const index = userAccount.findIndex(
-      (user) => user.username === username
-  );
+  let questionsFromLocalsrotage = JSON.parse(localStorage.getItem("questions"));
+  const totalQuestionsGrammaire =
+    questionsFromLocalsrotage[nevue].categories.Grammaire.length;
+  const totalQuestionsVocabulaire =
+    questionsFromLocalsrotage[nevue].categories.Vocabulaire.length;
+  const totalQuestionsCompréhension =
+    questionsFromLocalsrotage[nevue].categories.Compréhension.length;
+
+  const index = userAccount.findIndex((user) => user.username === username);
 
   // Get the current level data
-  const currentLevel = userAccount[index].levels[nevueName];
-  
-  // Calculate total score from all categories
-  const totalScore = [
-      currentLevel.Grammaire.noteCat || 0,
-      currentLevel.Vocabulaire.noteCat || 0,
-      currentLevel.Compréhension.noteCat || 0
-  ].reduce((sum, score) => sum + score, 0);
+  const noteGrammere = userAccount[index].levels[nevueName].Grammaire.noteCat;
+  const noteVocabulaire =
+    userAccount[index].levels[nevueName].Vocabulaire.noteCat;
+  const noteComprehnsion =
+    userAccount[index].levels[nevueName].Compréhension.noteCat;
+  const currentLevel = userAccount[index].levels;
 
+  userAccount[index].levels[nevueName].noteNiveau =
+    ((noteGrammere + noteVocabulaire + noteComprehnsion) * 10) /
+    (totalQuestionsGrammaire +
+      totalQuestionsVocabulaire +
+      totalQuestionsCompréhension);
+  // Calculate total score from all categories
+  const noteOfneveux = userAccount[index].levels[nevueName].noteNiveau;
   // Save total score to noteNiveau
-  userAccount[index].levels[nevueName].noteNiveau = totalScore;
-  
+  // userAccount[index].levels[nevueName].noteNiveau = totalScore
+
   // Save back to localStorage
   localStorage.setItem("userAccount", JSON.stringify(userAccount));
+
   
-  console.log(`Total score for ${nevueName}: ${totalScore}`);
-  return totalScore;
+
+  // return totalScore;
 }
+
+function openOtherneveu() {
+  let BtnNevA1 = document.getElementById("testA1");
+  let BtnNevA2 = document.getElementById("testA2");
+  let BtnNevB1 = document.getElementById("testB1");
+ 
+  let userAccount = JSON.parse(localStorage.getItem("userAccount"));
+  let username = localStorage.getItem("nameOFuser");
+  const index = userAccount.findIndex((user) => user.username === username);
+
+  const validGrammaireA1 = userAccount[index].levels.A1.Grammaire.valide ;
+  const validVocabulaireA1 = userAccount[index].levels.A1.Vocabulaire.valide ;
+  const validCompréhensionA1 = userAccount[index].levels.A1.Compréhension.valide ;
+
+  const validGrammaireA2 = userAccount[index].levels.A2.Grammaire.valide
+  const validVocabulaireA2 = userAccount[index].levels.A2.Vocabulaire.valide
+  const validCompréhensionA2 = userAccount[index].levels.A2.Compréhension.valide
+
+  const validGrammaireB1 = userAccount[index].levels.B1.Grammaire.valide
+  const validVocabulaireB1 = userAccount[index].levels.B1.Vocabulaire.valide
+  const validCompréhensionB1 = userAccount[index].levels.B1.Compréhension.valide
+
+  if(validCompréhensionA1 && validGrammaireA1 && validVocabulaireA1){
+    BtnNevA1.textContent =  BtnNevA1.textContent + "(valide)" ;
+    
+    BtnNevA2.disabled = false;
+    BtnNevA2.classList.toggle("cursor-not-allowed") ;
+    BtnNevA2.classList.add("bg-blue-500") ;
+    BtnNevA2.classList.remove("bg-gray-400") ;
+    
+
+  }
+  if(validCompréhensionA2 && validGrammaireA2 && validVocabulaireA2 && validCompréhensionA1 && validGrammaireA1 && validVocabulaireA1 ){
+    BtnNevA2.textContent =  BtnNevA2.textContent + "(valide)" ;
+    
+    BtnNevB1.disabled = false;
+    BtnNevB1.classList.toggle("cursor-not-allowed") ;
+    BtnNevB1.classList.add("bg-blue-500") ;
+    BtnNevB1.classList.remove("bg-gray-400") ;
+    
+
+  }
+  if(validGrammaireB1 && validVocabulaireB1 && validCompréhensionB1 && validCompréhensionA2 && validGrammaireA2 && validVocabulaireA2 && validCompréhensionA1 && validGrammaireA1 && validVocabulaireA1 ){
+    BtnNevB1.textContent =  BtnNevB1.textContent + "(valide)" ;
+    
+    // BtnNevB1.disabled = false;
+    // BtnNevB1.classList.toggle("cursor-not-allowed") ;
+    // BtnNevB1.classList.add("bg-blue-500") ;
+    // BtnNevB1.classList.remove("bg-gray-400") ;
+    
+
+  }
+  
+}
+openOtherneveu();
